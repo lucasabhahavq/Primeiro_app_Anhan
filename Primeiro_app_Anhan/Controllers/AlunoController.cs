aaaa0a9fa62c7e44e62f6d6fe4cef1a4a099852a
lucasabhahavq <lucasaraujoandrade23@gmail.com>
2026-03-11 17:19:08 -0300

    Adicionando projeto

---
+++ Primeiro_app_Anhan/Controllers/AlunoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Primeiro_app_Anhan.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Primeiro_app_Anhan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Primeiro_app_Anhan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
